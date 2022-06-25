a17dd8dff313e9a57d63df7379d85e73d36a5fbe
Joel Mathew V <joelmathewv0819@gmail.com>
2022-06-25 23:35:39 +0530

    basic api data in readable format

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const app = express();
 const https = require("https");
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=kottayam&appid=09b78a2117cd91872406ad6a8723d983&units=metric";
+    res.sendFile(__dirname + "/index.html");
+});
 
+app.post("/", function(req, res){
+    console.log("post received");
+    const place = req.body.fname;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + place + "&appid=09b78a2117cd91872406ad6a8723d983&units=metric";
     https.get(url, function(response){
         console.log(response.statusCode);
         response.on("data", function(data){
             const weatherdata = JSON.parse(data);
             console.log(weatherdata.main.temp);
-            res.send("<h1>the temperature is " + weatherdata.main.temp + " C</h1>");
+            res.send("<h1>the temperature at " + place + " is " + weatherdata.main.temp + " C</h1>");
         });
     });
-})
+});
+
+
 
 app.listen(3000, function(){
     console.log("server is running on port 3000");
