02d0c863552f5f16276773ff1a9b9d689c185719
Joel Mathew V <joelmathewv0819@gmail.com>
2022-06-26 09:39:51 +0530

    icons

--- app.js
+++ app.js
@@ -17,12 +17,16 @@ app.post("/", function(req, res){
         console.log(response.statusCode);
         response.on("data", function(data){
             const weatherdata = JSON.parse(data);
-            // console.log(weatherdata.main.temp);
-            // res.write(weatherdata.weather[0].description);
+            const icon = weatherdata.weather[0].icon;
+            const imgurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.set("Content-Type", "text/html");
+            res.write("<img src=" + imgurl + ">");
+            // console.log(weatherdata.weather[0].description);
+            res.write("<h1>" + weatherdata.weather[0].description + "</h1>");
             res.write("<h1>the temperature at " + place + " is " + weatherdata.main.temp + " C</h1>");
             res.write("<h1>the max temperature at " + place + " is " + weatherdata.main.temp_max + " C</h1>");
             res.write("<h1>the min temperature at " + place + " is " + weatherdata.main.temp_min + " C</h1>");
-            res.write("<h1>the humidity at " + place + " is " + weatherdata.main.humidity + " C</h1>");
+            res.write("<h1>the humidity at " + place + " is " + weatherdata.main.humidity + "</h1>");
             res.send();
         });
     });
